73bae22db6637d2fe9092a3c50dbfea684ee09f8
Paul Kasemir <paul.kasemir@gmail.com>
2026-02-13 14:27:05 -0700

    Initial commit

---
+++ Alchemy Filtering/Data Files/MWSE/mods/alchemyFiltering/strings.lua
@@ -0,0 +1,19 @@
+local strings = {
+	chooseEffects = "Choose Effects",
+	filterMatchingEffects = "Filter matching effects",
+	filteringEffect = "Filtering Effect:",
+}
+
+strings.mcm = {
+	modName = "Alchemy Filtering",
+	settings  = "Settings",
+
+	modEnabled = "Mod Status",
+	modEnabledDesc = "Enabling and disabling the mod and all its functionality",
+
+	chooserHeight = "Choose Effects Pane Height",
+	chooserHeightDesc = "Set the height in pixels for the choose effects pane that appears after clicking the " ..
+		strings.chooseEffects .. " button",
+}
+
+return strings
---
+++ README.md
@@ -0,0 +1,6 @@
+# Morrowind Mods
+
+This is a collection of mods written for The Elder Scrolls III: Morrowind.
+Each folder is the name of the Mod and includes the Data Files which can
+be installed into the Morrowind install folder or zipped up and installed
+using [Mod Organizer](https://www.nexusmods.com/skyrimspecialedition/mods/6194).
