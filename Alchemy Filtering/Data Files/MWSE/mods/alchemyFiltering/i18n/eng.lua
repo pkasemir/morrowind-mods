0debd686a86f8271afabf1fd79cf25f5351bc2c7
Paul Kasemir <paul.kasemir@gmail.com>
2026-03-13 16:36:40 -0600

    Rename strings to translations and subcategorize mcm label/desc

--- Alchemy Filtering/Data Files/MWSE/mods/alchemyFiltering/i18n/eng.lua
+++ Alchemy Filtering/Data Files/MWSE/mods/alchemyFiltering/i18n/eng.lua
@@ -1,4 +1,4 @@
-local strings = {
+local translations = {
     chooseEffects = "Choose Effects",
     chosenEffect = "Chosen Effect:",
 
@@ -13,22 +13,30 @@ local strings = {
     filterMatching = "Matching",
 }
 
-strings.mcm = {
+translations.mcm = {
     modName = "Alchemy Filtering",
     settings  = "Settings",
 
-    modEnabled = "Mod Status",
-    modEnabledDesc = "Enabling and disabling the mod and all its functionality",
+    modEnabled = {
+        label = "Mod Status",
+        desc = "Enabling and disabling the mod and all its functionality",
+    },
 
-    chosenEffectSticky = "Chosen Effect is Sticky",
-    chosenEffectStickyDesc = "Enabling this will make the previously chosen effect be selected when the Alchemy Menu is opened again",
+    chosenEffectSticky = {
+        label = "Chosen Effect is Sticky",
+        desc = "Enabling this will make the previously chosen effect be selected when the Alchemy Menu is opened again",
+    },
 
-    sortSticky = "Sort Order is Sticky",
-    sortStickyDesc = "Enabling this will make the previous sorting order in the inventory select menu be used when the parent menu is opened again",
+    sortSticky = {
+        label = "Sort Order is Sticky",
+        desc = "Enabling this will make the previous sorting order in the inventory select menu be used when the parent menu is opened again",
+    },
 
-    chooserHeight = "Choose Effects Pane Height",
-    chooserHeightDesc = "Set the height in pixels for the choose effects pane that appears after clicking the " ..
-    strings.chooseEffects .. " button",
+    chooserHeight = {
+        label = "Choose Effects Pane Height",
+        desc = "Set the height in pixels for the choose effects pane that appears after clicking the " ..
+            translations.chooseEffects .. " button",
+    },
 }
 
-return strings
+return translations
